a30aeac5a733adf354749e269e70a7ed1ce225d5
Radosław Miernik <radekmie@gmail.com>
2022-05-28 15:14:59 +0200

    Extracted syntax highlighting.

--- interpreter_node/src/ui/components/Editor.tsx
+++ interpreter_node/src/ui/components/Editor.tsx
@@ -1,105 +1,18 @@
 import { javascript } from '@codemirror/lang-javascript';
 import { json } from '@codemirror/lang-json';
-import { RangeSetBuilder } from '@codemirror/state';
-import {
-  Decoration,
-  DecorationSet,
-  EditorView,
-  ViewPlugin,
-  ViewUpdate,
-} from '@codemirror/view';
 import CodeMirror, { ReactCodeMirrorProps } from '@uiw/react-codemirror';
-import { BaseParser, Lexer } from 'chevrotain';
 
 import * as hrg from '../../hrg';
 import * as rg from '../../rg';
+import { createChevrotainHighlighter } from '../lib/createChevrotainHighlighter';
 import * as styles from './Application.module.css';
 import { Autosize } from './Autosize';
 
-const marks: Record<string, Decoration> = Object.create(null);
-function mark(tag: string) {
-  if (!(tag in marks)) {
-    marks[tag] = Decoration.mark({ attributes: { class: `ͼ${tag}` } });
-  }
-
-  return marks[tag];
-}
-
-function createChevrotainHighlighter(lexer: Lexer, parse: BaseParser) {
-  return ViewPlugin.fromClass(
-    class {
-      decorations: DecorationSet;
-
-      constructor(view: EditorView) {
-        this.decorations = this.getDecorations(view);
-      }
-
-      update({ docChanged, view }: ViewUpdate) {
-        if (docChanged) {
-          this.decorations = this.getDecorations(view);
-        }
-      }
-
-      getDecorations(view: EditorView) {
-        const marks: [number, number, string][] = [];
-        const source = view.state.doc.sliceString(0);
-
-        // Comments.
-        const lines = source.split('\n');
-        for (let index = 0; index < lines.length; ++index) {
-          const line = lines[index];
-          const position = line.indexOf('//');
-          if (position !== -1) {
-            const start = lines.slice(0, index).join('\n').length;
-            marks.push([start + position, start + line.length + 1, '5']);
-          }
-        }
-
-        // Lexing.
-        const { errors, tokens } = lexer.tokenize(source);
-        for (const {
-          image,
-          startOffset: start,
-          tokenType: { name },
-        } of tokens) {
-          const tag = name.startsWith('Keyword')
-            ? 'b'
-            : image === String(parseFloat(image))
-            ? 'd'
-            : name === 'Identifier' && image[0] === image[0].toUpperCase()
-            ? 'l'
-            : undefined;
-          if (tag) {
-            marks.push([start, start + image.length, tag]);
-          }
-        }
-
-        // Parsing.
-        // if (errors.length === 0) {
-        //   parser.input = tokens;
-        //   const cstNode = parser.GameDeclaration();
-        // }
-
-        // Build decorations.
-        marks.sort((x, y) => x[0] - y[0] || x[1] - y[1]);
-
-        const builder = new RangeSetBuilder<Decoration>();
-        for (const [start, end, tag] of marks) {
-          builder.add(start, end, mark(tag));
-        }
-
-        return builder.finish();
-      }
-    },
-    { decorations: instance => instance.decorations },
-  );
-}
-
 const modeToExtensions = {
-  hrg: [createChevrotainHighlighter(hrg.cst.lexer, hrg.cst.parser)],
+  hrg: [createChevrotainHighlighter(hrg.cst.lexer)],
   javascript: [javascript()],
   json: [json()],
-  rg: [createChevrotainHighlighter(rg.cst.lexer, rg.cst.parser)],
+  rg: [createChevrotainHighlighter(rg.cst.lexer)],
   text: [],
 };
 
