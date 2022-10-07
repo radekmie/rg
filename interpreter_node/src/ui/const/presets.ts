444bb3bf9b0d38757e3195f8b1ac0125c05ab491
Radosław Miernik <radekmie@gmail.com>
2022-10-07 20:27:35 +0200

    Added Amazon games to the UI.

--- interpreter_node/src/ui/const/presets.ts
+++ interpreter_node/src/ui/const/presets.ts
@@ -1,3 +1,5 @@
+import AmazonsOptimizedHL from 'bundle-text:../../../../examples/amazons-opt.hrg';
+import AmazonsHL from 'bundle-text:../../../../examples/amazons.hrg';
 import BreakthroughHL from 'bundle-text:../../../../examples/breakthrough.hrg';
 import BreakthroughLL from 'bundle-text:../../../../examples/breakthrough.rg';
 import Connect4HL from 'bundle-text:../../../../examples/connect4.hrg';
@@ -6,6 +8,8 @@ import TicTacToeLL from 'bundle-text:../../../../examples/ticTacToe.rg';
 import { Extension } from '../../types';
 
 export const presets = [
+  ['Amazons (HL)', AmazonsHL, Extension.hrg] as const,
+  ['Amazons optimized (HL)', AmazonsOptimizedHL, Extension.hrg] as const,
   ['Breakthrough (HL)', BreakthroughHL, Extension.hrg] as const,
   ['Breakthrough (LL)', BreakthroughLL, Extension.rg] as const,
   ['Connect4 (HL)', Connect4HL, Extension.hrg] as const,
