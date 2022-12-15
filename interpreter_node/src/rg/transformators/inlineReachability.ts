f5884417aaddc3936bd244a1f6ad1089eb54e1d5
Wojciech Jasiński <ilikeheaps@gmail.com>
2022-12-15 19:20:16 +0100

    WIP Implement inlineReachability

--- interpreter_node/src/rg/transformators/inlineReachability.ts
+++ interpreter_node/src/rg/transformators/inlineReachability.ts
@@ -2,5 +2,74 @@ import * as utils from '../../utils';
 import * as ast from '../ast';
 
 export function inlineReachability({ edges }: ast.GameDeclaration) {
+  function findThePath(start: ast.EdgeName, target: ast.EdgeName): ast.EdgeLabel[] | string {
+    var x: ast.EdgeName = start;
+    const visited: ast.EdgeName[] = [];
+    const path: ast.EdgeLabel[] = [];
+    // TODO when is '===' instead of 'isEqual' enough?
+    while (!utils.isEqual(x, target)) {
+      // no loops because they increase the number of possible paths
+      if (visited.some(y => utils.isEqual(y, x))) {
+        return "nope"
+      }
+
+      const reachable = edges.filter(e => utils.isEqual(e.lhs, x))
+
+      if (reachable.length != 1) {
+        return "nope"
+      }
+
+      const edge = reachable[0]
+      const l = edge.label
+
+      if (l.kind === 'Assignment') {
+        return "nope"
+      }
+
+      path.push(l);
+      visited.push(x);
+      x = edge.rhs;
+    }
+
+    return path;
+  }
+
+  function freshVar() {
+    // TODO
+    return ast.EdgeName({parts: [ast.Literal({ identifier: "foobar"})]});
+  }
+
+  function substitutePath(originalEdge: ast.EdgeDeclaration, path: ast.EdgeLabel[]) {
+    if (path.length == 0) {
+      originalEdge.label = ast.Skip({});
+      return;
+    }
+
+    const target = originalEdge.rhs;
+    
+    originalEdge.label = path[0];
+    originalEdge.rhs = freshVar();
+    
+    var last = originalEdge;
+    for (const l of path.slice(1)) {
+      const next = ast.EdgeDeclaration({ lhs: last.rhs, rhs: freshVar(), label: l });
+      edges.push(next);
+      last = next;
+    }
+
+    last.rhs = target;
+  }
+  
+  for (const e of edges) {
+    if (e.label.kind === 'Reachability') {
+      console.log("hello reachability")
+      const path = findThePath(e.lhs, e.rhs);
+      if(typeof path === 'object') {
+        substitutePath(e, path)
+      } else {
+        console.log("can't find path, msg:" + path)
+      }
+    }
+  }
   console.log("hello")
 }
