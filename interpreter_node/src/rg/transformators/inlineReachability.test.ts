3b4df55e117c4083e74f46c024fabeb82e8857c9
Wojciech Jasiński <ilikeheaps@gmail.com>
2022-12-29 21:10:53 +0100

    Add some more fine-grained tests for inlineReachability

---
+++ interpreter_node/src/rg/transformators/inlineReachability.test.ts
@@ -0,0 +1,234 @@
+import * as utils from '../../utils';
+import * as ast from '../ast';
+
+import * as t from './inlineReachability';
+
+function mkEdgeName(n: string): ast.EdgeName {
+  return ast.EdgeName({ parts: [ast.Literal({ identifier: n })] });
+}
+
+function edge(
+  lhs: ast.EdgeName,
+  rhs: ast.EdgeName,
+  label: ast.EdgeLabel,
+): ast.EdgeDeclaration {
+  return ast.EdgeDeclaration({ lhs: lhs, rhs: rhs, label: label });
+}
+
+function mkEdge(
+  lhs: string,
+  rhs: string,
+  label: ast.EdgeLabel,
+): ast.EdgeDeclaration {
+  return edge(mkEdgeName(lhs), mkEdgeName(rhs), label);
+}
+
+function mkEdges(chain: ast.EdgeName[]): ast.EdgeDeclaration[] {
+  return [...Array(chain.length - 1).keys()].map(i =>
+    edge(chain[i], chain[i + 1], ast.Skip({})),
+  );
+}
+
+function mkLabel(n: number): ast.EdgeLabel {
+  return ast.Assignment({
+    lhs: ast.Reference({ identifier: 'x' + n.toString() }),
+    rhs: ast.Reference({ identifier: 'y' }),
+  });
+}
+
+describe('inlineReachability', () => {
+  test('findThePath should find a simple path in a chain', () => {
+    let nodes = ['x', 'y', 'z'].map(mkEdgeName);
+    let edges = mkEdges(nodes);
+
+    expect(t.findThePath(edges, nodes[0], nodes.reverse()[0]))
+      .toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "kind": "Skip",
+        },
+        Object {
+          "kind": "Skip",
+        },
+      ]
+    `);
+  });
+
+  test('findThePath should reject multiple paths', () => {
+    let nodes1 = ['x', 'y', 'z'].map(mkEdgeName);
+    let nodes2 = ['x', 'y1', 'z'].map(mkEdgeName);
+    let edges = mkEdges(nodes1).concat(mkEdges(nodes2));
+
+    expect(
+      t.findThePath(edges, nodes1[0], nodes1.reverse()[0]),
+    ).toMatchInlineSnapshot(`"nope"`);
+  });
+
+  test('substitutePath should reuse edge when substituting single edge', () => {
+    let nodes = ['x', 'y', 'z'].map(mkEdgeName);
+    let edges = mkEdges(nodes);
+
+    t.substitutePath(edges, edges[0], [mkLabel(1)]);
+
+    expect(edges).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "kind": "EdgeDeclaration",
+          "label": Object {
+            "kind": "Assignment",
+            "lhs": Object {
+              "identifier": "x1",
+              "kind": "Reference",
+            },
+            "rhs": Object {
+              "identifier": "y",
+              "kind": "Reference",
+            },
+          },
+          "lhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "x",
+                "kind": "Literal",
+              },
+            ],
+          },
+          "rhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "y",
+                "kind": "Literal",
+              },
+            ],
+          },
+        },
+        Object {
+          "kind": "EdgeDeclaration",
+          "label": Object {
+            "kind": "Skip",
+          },
+          "lhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "y",
+                "kind": "Literal",
+              },
+            ],
+          },
+          "rhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "z",
+                "kind": "Literal",
+              },
+            ],
+          },
+        },
+      ]
+    `);
+  });
+
+  test('substitutePath should create one edge when substituting two', () => {
+    let nodes = ['x', 'y', 'z'].map(mkEdgeName);
+    let edges = mkEdges(nodes);
+
+    t.substitutePath(edges, edges[0], [1, 2].map(mkLabel));
+
+    expect(edges).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "kind": "EdgeDeclaration",
+          "label": Object {
+            "kind": "Assignment",
+            "lhs": Object {
+              "identifier": "x1",
+              "kind": "Reference",
+            },
+            "rhs": Object {
+              "identifier": "y",
+              "kind": "Reference",
+            },
+          },
+          "lhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "x",
+                "kind": "Literal",
+              },
+            ],
+          },
+          "rhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "__gen_1",
+                "kind": "Literal",
+              },
+            ],
+          },
+        },
+        Object {
+          "kind": "EdgeDeclaration",
+          "label": Object {
+            "kind": "Skip",
+          },
+          "lhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "y",
+                "kind": "Literal",
+              },
+            ],
+          },
+          "rhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "z",
+                "kind": "Literal",
+              },
+            ],
+          },
+        },
+        Object {
+          "kind": "EdgeDeclaration",
+          "label": Object {
+            "kind": "Assignment",
+            "lhs": Object {
+              "identifier": "x2",
+              "kind": "Reference",
+            },
+            "rhs": Object {
+              "identifier": "y",
+              "kind": "Reference",
+            },
+          },
+          "lhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "__gen_1",
+                "kind": "Literal",
+              },
+            ],
+          },
+          "rhs": Object {
+            "kind": "EdgeName",
+            "parts": Array [
+              Object {
+                "identifier": "y",
+                "kind": "Literal",
+              },
+            ],
+          },
+        },
+      ]
+    `);
+  });
+});
