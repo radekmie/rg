96db2d85f5fc290f094873b4ced08c8039a57969
Radosław Miernik <radekmie@gmail.com>
2023-01-12 21:45:29 +0100

    Added parse benchmark.

---
+++ interpreter_node/src/bench.test.ts
@@ -0,0 +1,49 @@
+import fs from 'fs';
+import path from 'path';
+
+import { parse } from './parse';
+import { Extension, Flag, noFlagsEnabled } from './types';
+import * as utils from './utils';
+
+// These are _really_ heavy.
+describe.skip('bench', () => {
+  const examplesPath = path.join(__dirname, '..', '..', 'examples');
+  const examples = fs
+    .readdirSync(examplesPath)
+    .sort()
+    .flatMap(fileName => {
+      const filePath = path.join(examplesPath, fileName);
+      const extension = path.extname(filePath);
+      if (
+        extension !== Extension.hrg &&
+        extension !== Extension.rbg &&
+        extension !== Extension.rg
+      ) {
+        return [];
+      }
+
+      const source = fs.readFileSync(filePath, { encoding: 'utf8' });
+      return [{ extension, fileName, source }];
+    });
+
+  const flagNames = Object.keys(noFlagsEnabled) as Flag[];
+  for (const flagBits of flagNames
+    .map(() => [false, true])
+    .reduce<boolean[][]>(utils.cartesian, [[]])) {
+    const flagsSelected = flagNames.filter((flag, index) => flagBits[index]);
+    const flags = flagsSelected.reduce(
+      (flags, flag) => {
+        flags[flag] = true;
+        return flags;
+      },
+      { ...noFlagsEnabled },
+    );
+
+    for (const { extension, fileName, source } of examples) {
+      const args = flagsSelected.map(flag => `--${flag}`).join(' ');
+      test(`${fileName} ${args}`, () => {
+        parse(source, { extension, flags });
+      });
+    }
+  }
+});
