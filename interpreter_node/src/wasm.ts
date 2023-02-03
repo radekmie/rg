0666024312cd1662e9d4dc44e296b60f141ca53c
Radosław Miernik <radekmie@gmail.com>
2023-02-03 20:54:21 +0100

    Added polyfill for crypto in Node.js.

--- interpreter_node/src/wasm.ts
+++ interpreter_node/src/wasm.ts
@@ -4,6 +4,12 @@ import { readFileSync } from 'fs';
 import init from './wasm-module';
 export * from './wasm-module';
 
+// Node.js requires a crypto polyfill. Importing it directly inlines it in the
+// browser too, but we don't need it there.
+if (typeof window === 'undefined') {
+  eval("globalThis.crypto = require('crypto').webcrypto;");
+}
+
 // WASM module is inlined in the browser and referenced in the CLI version.
 const buffer = readFileSync(__dirname + '/wasm-module/index_bg.wasm');
 export const initPromise = init(buffer);
