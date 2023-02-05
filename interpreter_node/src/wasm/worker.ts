b84623e43af4a3e52481f210a08fd1a0a405b042
Radosław Miernik <radekmie@gmail.com>
2023-02-06 00:41:01 +0100

    Improved WebWorker.

--- interpreter_node/src/wasm/worker.ts
+++ interpreter_node/src/wasm/worker.ts
@@ -1,6 +1,6 @@
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
 
-import { initSync, parseRg, perfRg, runRg } from './module';
+import init, { parseRg, perfRg, runRg } from './module';
 
 // Node.js requires a crypto polyfill. Importing it directly inlines it in the
 // browser too, but we don't need it there. Yep, this is a nasty `eval` trick.
@@ -8,21 +8,25 @@ if (typeof crypto === 'undefined') {
   eval("globalThis.crypto = require('crypto').webcrypto;");
 }
 
-// WASM module is inlined in the browser and referenced in the CLI version.
-initSync(readFileSync(__dirname + '/module/index_bg.wasm'));
+const url = new URL('./module/index_bg.wasm', import.meta.url);
+const response = url.protocol === 'file:' ? readFile(url.pathname) : fetch(url);
+const initPromise = init(response);
+initPromise.catch(console.error);
 
 const methods = { parseRg, perfRg, runRg };
 self.addEventListener('message', ({ data }) => {
-  try {
-    self.postMessage({
-      done: true,
-      // @ts-expect-error Check `index.ts` for details.
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access -- Check `index.ts` for details.
-      value: methods[data.fn](...data.args, (...value) => {
-        self.postMessage({ done: false, value });
-      }),
+  initPromise
+    .then(() => {
+      self.postMessage({
+        done: true,
+        // @ts-expect-error Check `index.ts` for details.
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access -- Check `index.ts` for details.
+        value: methods[data.fn](...data.args, (...value) => {
+          self.postMessage({ done: false, value });
+        }),
+      });
+    })
+    .catch(error => {
+      self.postMessage({ error });
     });
-  } catch (error) {
-    self.postMessage({ error });
-  }
 });
