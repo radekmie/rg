6ab06cdb8d8ed842af40776b5889d984d62825d4
Radosław Miernik <radekmie@gmail.com>
2023-03-06 21:12:16 +0100

    Fixed --normalizeTypes.

--- interpreter_rust/rg/src/ast_transforms/normalize_types.rs
+++ interpreter_rust/rg/src/ast_transforms/normalize_types.rs
@@ -35,55 +35,25 @@ impl Type<Rc<str>> {
             return Ok(self.clone());
         }
 
-        let self_normalized = match self {
-            Self::Arrow { lhs, rhs } => Self::Arrow {
-                lhs: lhs.clone(),
-                rhs: Rc::new(rhs.normalize(game)?),
-            },
-            _ => self.clone(),
-        };
-
+        let self_normalized = self.normalize_direct(game)?;
         if let Some(typedef) = game.resolve_typedef_by_type(&self_normalized)? {
             return Ok(Self::TypeReference {
                 identifier: typedef.identifier.clone(),
             });
         }
 
-        let normalized_type_name = "NormalizedType";
-        let identifier = match self_normalized {
-            Self::Arrow { ref lhs, ref rhs } => match &**rhs {
-                Self::TypeReference { identifier } => Some(format!("{lhs}_{identifier}")),
-                _ => None,
-            },
-            _ => None,
-        }
-        .unwrap_or_else(|| normalized_type_name.to_string());
-
-        let mut index: Option<usize> = None;
+        let mut index = 1;
         let identifier = loop {
-            let identifier_with_index = Rc::from(format!(
-                "{identifier}{}",
-                index.map_or("".to_string(), |index| index.to_string())
-            ));
-
+            let identifier = Rc::from(format!("Type{}", index));
             if !game
                 .typedefs
                 .iter()
-                .any(|typedef| typedef.identifier == identifier_with_index)
+                .any(|typedef| typedef.identifier == identifier)
             {
-                break identifier_with_index;
+                break identifier;
             }
 
-            index = Some(index.map_or_else(
-                || {
-                    if identifier == normalized_type_name {
-                        1
-                    } else {
-                        2
-                    }
-                },
-                |index| index + 1,
-            ));
+            index += 1;
         };
 
         game.typedefs.push(Typedef {
@@ -93,20 +63,24 @@ impl Type<Rc<str>> {
 
         Ok(Self::TypeReference { identifier })
     }
+
+    fn normalize_direct(&self, game: &mut Game<Rc<str>>) -> Result<Self, Error<Rc<str>>> {
+        Ok(match self {
+            Self::Arrow { lhs, rhs } => Self::Arrow {
+                lhs: Rc::new(lhs.normalize(game)?),
+                rhs: Rc::new(rhs.normalize(game)?),
+            },
+            _ => self.clone(),
+        })
+    }
 }
 
 impl Typedef<Rc<str>> {
     pub fn normalize_type(&self, game: &mut Game<Rc<str>>) -> Result<Self, Error<Rc<str>>> {
-        match &*self.type_ {
-            Type::Arrow { lhs, rhs } => Ok(Self {
-                identifier: self.identifier.clone(),
-                type_: Rc::new(Type::Arrow {
-                    lhs: lhs.clone(),
-                    rhs: Rc::new(rhs.normalize(game)?),
-                }),
-            }),
-            _ => Ok(self.clone()),
-        }
+        Ok(Self {
+            identifier: self.identifier.clone(),
+            type_: Rc::new(self.type_.normalize_direct(game)?),
+        })
     }
 }
 
@@ -118,4 +92,73 @@ impl Variable<Rc<str>> {
             type_: Rc::new(self.type_.normalize(game)?),
         })
     }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ast::Game;
+    use crate::parser::game;
+    use map_id::MapId;
+    use nom::combinator::all_consuming;
+    use std::rc::Rc;
+
+    fn parse(input: &str) -> Game<Rc<str>> {
+        let (_, game) = all_consuming(game)(input).unwrap();
+        game.map_id(&mut |id| Rc::from(*id))
+    }
+
+    macro_rules! test {
+        ($name:ident { $($actual:tt)* } { $($expect:tt)* }) => {
+            #[test]
+            fn $name() {
+                let mut actual = parse(stringify!($($actual)*));
+                actual.normalize_types().unwrap();
+                let expect = parse(stringify!($($expect)*));
+
+                assert_eq!(actual, expect, "\n\n>>> Actual: <<<\n{actual}\n>>> Expect: <<<\n{expect}\n");
+            }
+        };
+    }
+
+    test!(
+        set
+        { type X = { a }; }
+        { type X = { a }; }
+    );
+
+    test!(
+        arrow2
+        { type X = { a } -> { b }; }
+        {
+            type X = Type1 -> Type2;
+            type Type1 = { a };
+            type Type2 = { b };
+        }
+    );
+
+    test!(
+        arrow3
+        { type X = { a } -> { b } -> { c }; }
+        {
+            type X = Type1 -> Type4;
+            type Type1 = { a };
+            type Type2 = { b };
+            type Type3 = { c };
+            type Type4 = Type2 -> Type3;
+        }
+    );
+
+    test!(
+        arrow4
+        { type X = { a } -> { b } -> { c } -> { d }; }
+        {
+            type X = Type1 -> Type6;
+            type Type1 = { a };
+            type Type2 = { b };
+            type Type3 = { c };
+            type Type4 = { d };
+            type Type5 = Type3 -> Type4;
+            type Type6 = Type2 -> Type5;
+        }
+    );
 }
