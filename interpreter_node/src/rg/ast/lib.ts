dafb5e5ee59db42dce95508bd8f5ccf5cb720b7e
Radosław Miernik <radekmie@gmail.com>
2023-03-24 22:51:51 +0100

    Added fast paths for Node.js RG lib.

--- interpreter_node/src/rg/ast/lib.ts
+++ interpreter_node/src/rg/ast/lib.ts
@@ -55,6 +55,17 @@ export function hasBindings({ parts }: ast.EdgeName) {
 }
 
 export function incoming(edges: ast.EdgeDeclaration[], edgeName: ast.EdgeName) {
+  // Fast path for simple edge names.
+  if (edgeName.parts.length === 1 && edgeName.parts[0].kind === 'Literal') {
+    const { identifier } = edgeName.parts[0];
+    return edges.filter(
+      ({ rhs: { parts } }) =>
+        parts.length === 1 &&
+        parts[0].kind === 'Literal' &&
+        parts[0].identifier === identifier,
+    );
+  }
+
   return edges.filter(({ rhs }) => utils.isEqual(edgeName, rhs));
 }
 
@@ -123,6 +134,17 @@ export function makeFreshName(
 }
 
 export function outgoing(edges: ast.EdgeDeclaration[], edgeName: ast.EdgeName) {
+  // Fast path for simple edge names.
+  if (edgeName.parts.length === 1 && edgeName.parts[0].kind === 'Literal') {
+    const { identifier } = edgeName.parts[0];
+    return edges.filter(
+      ({ lhs: { parts } }) =>
+        parts.length === 1 &&
+        parts[0].kind === 'Literal' &&
+        parts[0].identifier === identifier,
+    );
+  }
+
   return edges.filter(({ lhs }) => utils.isEqual(edgeName, lhs));
 }
 
