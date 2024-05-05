0d5831149af7c43678ce5edd4f8f72f7ad0e0076
Radosław Miernik <radekmie@gmail.com>
2024-05-05 18:53:21 +0200

    Added more RBG games.

--- interpreter_node/src/ui/const/presets.ts
+++ interpreter_node/src/ui/const/presets.ts
@@ -1,10 +1,12 @@
 import AmazonsNaiveHrg from 'bundle-text:../../../../examples/amazons-naive.hrg';
 import AmazonsSmartHrg from 'bundle-text:../../../../examples/amazons-smart.hrg';
+import AmazonsRbg from 'bundle-text:../../../../examples/amazons.rbg';
 import BreakthroughHrg from 'bundle-text:../../../../examples/breakthrough.hrg';
 import BreakthroughRbg from 'bundle-text:../../../../examples/breakthrough.rbg';
 import BreakthroughRg from 'bundle-text:../../../../examples/breakthrough.rg';
 import Connect4Hrg from 'bundle-text:../../../../examples/connect4.hrg';
 import Connect4Gdl from 'bundle-text:../../../../examples/connect4.kif';
+import Connect4Rbg from 'bundle-text:../../../../examples/connect4.rbg';
 import Hex2Rbg from 'bundle-text:../../../../examples/hex2.rbg';
 import Hex9Rbg from 'bundle-text:../../../../examples/hex9.rbg';
 import KnightthroughHrg from 'bundle-text:../../../../examples/knightthrough.hrg';
@@ -18,11 +20,13 @@ import { Language } from '../../types';
 export const presets = [
   ['Amazons (naive)', AmazonsNaiveHrg, Language.hrg] as const,
   ['Amazons (smart)', AmazonsSmartHrg, Language.hrg] as const,
+  ['Amazons', AmazonsRbg, Language.rbg] as const,
   ['Breakthrough', BreakthroughHrg, Language.hrg] as const,
   ['Breakthrough', BreakthroughRbg, Language.rbg] as const,
   ['Breakthrough', BreakthroughRg, Language.rg] as const,
   ['Connect4', Connect4Hrg, Language.hrg] as const,
   ['Connect4', Connect4Gdl, Language.gdl] as const,
+  ['Connect4', Connect4Rbg, Language.rbg] as const,
   ['Hex2', Hex2Rbg, Language.rbg] as const,
   ['Hex9', Hex9Rbg, Language.rbg] as const,
   ['Knightthrough', KnightthroughHrg, Language.hrg] as const,
