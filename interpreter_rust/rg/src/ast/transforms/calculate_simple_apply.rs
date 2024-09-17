b4703e0bc936067de4395c31bbb0beec6795475f
Radosław Miernik <radekmie@gmail.com>
2024-09-18 00:17:02 +0200

    Merged some instances of @simpleApply.

--- interpreter_rust/rg/src/ast/transforms/calculate_simple_apply.rs
+++ interpreter_rust/rg/src/ast/transforms/calculate_simple_apply.rs
@@ -25,7 +25,7 @@ impl Game<Arc<str>> {
         let mut pragmas = vec![];
         'outer: for node in nodes {
             let mut paths_to_edges = BTreeMap::new();
-            let mut paths_to_players: BTreeMap<_, BTreeSet<(_, _, _)>> = BTreeMap::new();
+            let mut paths_to_players: BTreeMap<_, _> = BTreeMap::new();
             let mut paths_to_tags: BTreeMap<_, BTreeSet<(_, _, _)>> = BTreeMap::new();
             let mut queue = vec![(node.clone(), vec![], vec![])];
             while let Some((lhs, path, assignments)) = queue.pop() {
@@ -52,24 +52,20 @@ impl Game<Arc<str>> {
                                 Label::Assignment { lhs, rhs } => {
                                     assignments.push(edge.label.clone());
                                     if lhs.uncast().is_player_reference() {
-                                        paths_to_players.entry(rhs.clone()).or_default().insert((
-                                            edge.rhs.clone(),
-                                            path.clone(),
-                                            assignments,
-                                        ));
+                                        paths_to_players.entry(rhs.clone()).or_insert(path);
                                     } else {
-                                        queue.push((edge.rhs.clone(), path.clone(), assignments));
+                                        queue.push((edge.rhs.clone(), path, assignments));
                                     }
                                 }
                                 Label::Tag { symbol } => {
                                     paths_to_tags.entry(symbol.clone()).or_default().insert((
                                         edge.rhs.clone(),
-                                        path.clone(),
+                                        path,
                                         assignments,
                                     ));
                                 }
                                 _ => {
-                                    queue.push((edge.rhs.clone(), path.clone(), assignments));
+                                    queue.push((edge.rhs.clone(), path, assignments));
                                 }
                             }
                         }
@@ -79,34 +75,50 @@ impl Game<Arc<str>> {
 
             if paths_to_players.len() <= 1 {
                 if paths_to_players.len() == 1 {
-                    pragmas.push(Pragma::SimpleApply {
-                        span: Span::none(),
-                        node: node.clone(),
-                        tags: vec![],
-                        nodes: paths_to_players
-                            .pop_first()
-                            .unwrap()
-                            .1
-                            .pop_first()
-                            .unwrap()
-                            .1,
-                    });
+                    pragmas.push((node.clone(), paths_to_players.pop_first().unwrap().1, None));
                 }
 
                 for (tag, mut paths) in paths_to_tags.into_iter() {
                     if paths.len() == 1 {
-                        pragmas.push(Pragma::SimpleApply {
-                            span: Span::none(),
-                            node: node.clone(),
-                            tags: vec![tag],
-                            nodes: paths.pop_first().unwrap().1,
-                        });
+                        pragmas.push((node.clone(), paths.pop_first().unwrap().1, Some(tag)));
                     }
                 }
             }
         }
 
-        for pragma in pragmas {
+        // Merge all pairs of
+        //   @simpleApply x : ...xs;
+        //   @simpleApply y ...ts : ...ys x;
+        // Into
+        //   @simpleApply y ...ts : ...ys x ...xs;
+        // If there's exactly one `@simpleApply x : ...;`.
+        for index_x in (1..pragmas.len()).rev() {
+            let (prev, next) = pragmas.split_at_mut(index_x);
+            let ((x, xs, tag), next) = next.split_first_mut().unwrap();
+            if tag.is_some() || prev.iter().chain(next.iter()).any(|(node, _, _)| node == x) {
+                continue;
+            }
+
+            let mut any_matched = false;
+            for (_, ys, _) in prev.iter_mut().chain(next) {
+                if ys.last() == Some(x) {
+                    ys.extend_from_slice(xs);
+                    any_matched = true;
+                }
+            }
+
+            if any_matched {
+                pragmas.swap_remove(index_x);
+            }
+        }
+
+        for (node, nodes, tag) in pragmas {
+            let pragma = Pragma::SimpleApply {
+                span: Span::none(),
+                node,
+                tags: tag.into_iter().collect(),
+                nodes,
+            };
             if let Err(index) = self.pragmas.binary_search(&pragma) {
                 self.pragmas.insert(index, pragma);
             }
