e6735a338672183e166a0ac95c21256eaa16e097
Radosław Miernik <radekmie@gmail.com>
2024-09-25 19:22:30 +0200

    Improved --calculateSimpleApply.

--- interpreter_rust/rg/src/ast/transforms/calculate_simple_apply.rs
+++ interpreter_rust/rg/src/ast/transforms/calculate_simple_apply.rs
@@ -23,74 +23,61 @@ impl Game<Arc<str>> {
             .collect();
 
         let mut pragmas = vec![];
-        'outer: for node in nodes {
-            let mut paths_to_edges = BTreeMap::new();
+        for node in nodes {
+            let mut paths_to_edges: BTreeMap<_, BTreeSet<_>> = BTreeMap::new();
             let mut paths_to_players: BTreeMap<_, _> = BTreeMap::new();
             let mut paths_to_tags: BTreeMap<_, BTreeSet<(_, _, _)>> = BTreeMap::new();
             let mut queue = vec![(node.clone(), vec![], vec![])];
             while let Some((lhs, path, assignments)) = queue.pop() {
-                let maybe_edges = next_edges.get(&lhs);
+                let path_to_edge = paths_to_edges.entry(lhs.clone()).or_default();
 
-                let mut seen = None;
-                paths_to_edges
-                    .entry(lhs.clone())
-                    .and_modify(|existing| seen = Some(*existing == assignments))
-                    .or_insert_with(|| assignments.clone());
+                // Add only if it's not saturated yet.
+                if path_to_edge.len() <= 1 {
+                    path_to_edge.insert(assignments.clone());
+                }
 
-                match seen {
-                    Some(true) => {}
-                    Some(false) => continue 'outer,
-                    None => {
-                        let Some(edges) = maybe_edges else { continue };
+                for edge in next_edges.get(&lhs).into_iter().flatten() {
+                    let mut path = path.clone();
+                    path.push(edge.rhs.clone());
 
-                        for edge in edges {
-                            let mut path = path.clone();
-                            path.push(edge.rhs.clone());
-
-                            let mut assignments = assignments.clone();
-                            match &edge.label {
-                                Label::Assignment { lhs, rhs } => {
-                                    assignments.push(edge.label.clone());
-                                    if lhs.uncast().is_player_reference() {
-                                        paths_to_players.entry(rhs.clone()).or_insert(path);
-                                    } else {
-                                        queue.push((edge.rhs.clone(), path, assignments));
-                                    }
-                                }
-                                Label::Tag { symbol } => {
-                                    paths_to_tags.entry(symbol.clone()).or_default().insert((
-                                        edge.rhs.clone(),
-                                        path,
-                                        assignments,
-                                    ));
-                                }
-                                _ => {
-                                    queue.push((edge.rhs.clone(), path, assignments));
-                                }
+                    let mut assignments = assignments.clone();
+                    match &edge.label {
+                        Label::Assignment { lhs, rhs } => {
+                            assignments.push(edge.label.clone());
+                            if lhs.uncast().is_player_reference() {
+                                paths_to_players.entry(rhs.clone()).or_insert(path);
+                            } else {
+                                queue.push((edge.rhs.clone(), path, assignments));
                             }
+                        }
+                        Label::Tag { symbol } => {
+                            paths_to_tags.entry(symbol.clone()).or_default().insert((
+                                edge.rhs.clone(),
+                                path,
+                                assignments,
+                            ));
+                        }
+                        _ => {
+                            queue.push((edge.rhs.clone(), path, assignments));
                         }
                     }
                 }
             }
 
             if paths_to_players.len() <= 1 {
+                let is_exhaustive = paths_to_edges
+                    .values()
+                    .all(|assignments| assignments.len() == 1);
+
                 if paths_to_players.len() == 1 {
-                    pragmas.push((
-                        node.clone(),
-                        vec![],
-                        paths_to_players.pop_first().unwrap().1,
-                        true,
-                    ));
+                    let path = paths_to_players.pop_first().unwrap().1;
+                    pragmas.push((node.clone(), vec![], path, true, is_exhaustive));
                 }
 
-                for (tag, mut paths) in paths_to_tags.into_iter() {
+                for (tag, mut paths) in paths_to_tags {
                     if paths.len() == 1 {
-                        pragmas.push((
-                            node.clone(),
-                            vec![tag],
-                            paths.pop_first().unwrap().1,
-                            false,
-                        ));
+                        let path = paths.pop_first().unwrap().1;
+                        pragmas.push((node.clone(), vec![tag], path, false, is_exhaustive));
                     }
                 }
             }
@@ -105,18 +92,18 @@ impl Game<Arc<str>> {
         // `player` assignment merged on the resulting path (except the end).
         for index_x in (0..pragmas.len()).rev() {
             let (prev, next) = pragmas.split_at_mut(index_x);
-            let ((x, xtags, xs, xplayer), next) = next.split_first_mut().unwrap();
+            let ((x, xtags, xs, xplayer, _), next) = next.split_first_mut().unwrap();
             if prev
                 .iter()
                 .chain(next.iter())
-                .any(|(node, _, _, _)| node == x)
+                .any(|(node, _, _, _, _)| node == x)
             {
                 continue;
             }
 
             let mut any_matched = false;
-            for (_, ytags, ys, yplayer) in prev.iter_mut().chain(next) {
-                if ys.last() == Some(x) && (!*xplayer || !*yplayer) {
+            for (_, ytags, ys, yplayer, _) in prev.iter_mut().chain(next) {
+                if ys.last() == Some(x) && !*yplayer {
                     ytags.extend_from_slice(xtags);
                     ys.extend_from_slice(xs);
                     *yplayer |= *xplayer;
@@ -129,12 +116,21 @@ impl Game<Arc<str>> {
             }
         }
 
-        for (node, tags, nodes, _) in pragmas {
-            let pragma = Pragma::SimpleApply {
-                span: Span::none(),
-                node,
-                tags,
-                nodes,
+        for (node, tags, nodes, _, is_exhaustive) in pragmas {
+            let pragma = if is_exhaustive {
+                Pragma::SimpleApplyExhaustive {
+                    span: Span::none(),
+                    node,
+                    tags,
+                    nodes,
+                }
+            } else {
+                Pragma::SimpleApply {
+                    span: Span::none(),
+                    node,
+                    tags,
+                    nodes,
+                }
             };
 
             if let Err(index) = self.pragmas.binary_search(&pragma) {
@@ -152,33 +148,38 @@ mod test {
 
     test_transform!(
         calculate_simple_apply,
-        complext_1,
+        complex_1,
         include_str!("../../../../../examples/simpleApplyTest1.rg"),
         adds "
-            @simpleApply doneB dummytag : extraB preend;
-            @simpleApply moveA 0 : tagA0 doneA moveB;
-            @simpleApply moveA 1 : tagA1 doneA moveB;
-            @simpleApply preend : end;
+            @simpleApplyExhaustive doneB dummytag : extraB preend;
+            @simpleApplyExhaustive moveA 0 : tagA0 doneA moveB;
+            @simpleApplyExhaustive moveA 1 : tagA1 doneA moveB;
+            @simpleApplyExhaustive preend : end;
         "
     );
 
     test_transform!(
         calculate_simple_apply,
-        complext_2,
+        complex_2,
         include_str!("../../../../../examples/simpleApplyTest2.rg"),
         adds "
-            @simpleApply moveA 0 : tagA0 doneA moveB;
-            @simpleApply moveA 1 : tagA1 doneA moveB;
-            @simpleApply moveB 0 dummytag : tagB0same tagB0 doneB extraB preend;
-            @simpleApply moveB 1 dummytag : tagB1same tagB1 doneB extraB preend;
-            @simpleApply preend : end;
+            @simpleApplyExhaustive moveA 0 : tagA0 doneA moveB;
+            @simpleApplyExhaustive moveA 1 : tagA1 doneA moveB;
+            @simpleApplyExhaustive moveB 0 dummytag : tagB0same tagB0 doneB extraB preend;
+            @simpleApplyExhaustive moveB 1 dummytag : tagB1same tagB1 doneB extraB preend;
+            @simpleApplyExhaustive preend : end;
         "
     );
 
-    // test_transform!(
-    //     calculate_simple_apply,
-    //     complext_3,
-    //     include_str!("../../../../../examples/simpleApplyTest3.rg"),
-    //     adds "@simpleApply moveB 1 : tagB1same tagB1 doneB extraB preend;"
-    // );
+    test_transform!(
+        calculate_simple_apply,
+        complex_3,
+        include_str!("../../../../../examples/simpleApplyTest3.rg"),
+        adds "
+            @simpleApplyExhaustive moveA 0 : tagA0 doneA moveB;
+            @simpleApplyExhaustive moveA 1 : tagA1 doneA moveB;
+            @simpleApply moveB 1 dummytag : tagB1same tagB1 doneB extraB preend;
+            @simpleApplyExhaustive preend : end;
+        "
+    );
 }
