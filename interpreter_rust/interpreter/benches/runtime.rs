160d4640f2288d713102a5133636fc33fbc8f66b
Radosław Miernik <radekmie@gmail.com>
2024-10-03 21:39:06 +0200

    Reworked benchmarks.

--- interpreter_rust/interpreter/benches/runtime.rs
+++ interpreter_rust/interpreter/benches/runtime.rs
@@ -1,39 +1,36 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use interpreter::ist::{Game, RuntimeId};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use interpreter::{analyze_rg_inner, prepare_ist, Flags};
+use rand::rngs::SmallRng;
+use rand::SeedableRng;
 use std::fs::read_to_string;
+use std::time::Duration;
 
-fn prepare(path: &str, flags: Flags) -> Game<RuntimeId> {
-    let source = read_to_string(format!("../../examples/{path}")).unwrap();
-    let game = analyze_rg_inner(source.as_str(), &flags, None::<fn(_)>).unwrap();
-    prepare_ist(game).unwrap().0
-}
+fn scenario(criterion: &mut Criterion, name: &str, variants: &[&str]) {
+    let mut rng = SmallRng::seed_from_u64(0);
+    let mut group = criterion.benchmark_group(format!("runtime/{name}"));
+    group.measurement_time(Duration::from_millis(400));
+    group.warm_up_time(Duration::from_millis(100));
 
-fn scenario(criterion: &mut Criterion, path: &str, counts: Vec<usize>) {
-    let mut group = criterion.benchmark_group(format!("runtime/{path}"));
-
-    macro_rules! bench {
-        ($name:ident, $flags:expr) => {
-            let game = prepare(path, $flags);
-            group.bench_function(stringify!($name), |bencher| {
-                bencher.iter(|| {
-                    for (depth, expected_count) in counts.iter().enumerate() {
-                        game.perf(depth, &|count| {
-                            assert_eq!(count, *expected_count);
-                        });
-                    }
-                });
-            });
-        };
+    for variant in variants {
+        let source = read_to_string(format!("../../examples/{name}{variant}")).unwrap();
+        for (flags_name, flags) in [("all", Flags::all()), ("none", Flags::none())] {
+            let ast = analyze_rg_inner(source.as_str(), &flags, None::<fn(_)>).unwrap();
+            let ist = prepare_ist(ast).unwrap().0;
+            group.bench_function(
+                BenchmarkId::new(variant.to_string(), flags_name),
+                |bencher| bencher.iter(|| ist.run(&mut rng, 1, &|_| {})),
+            );
+        }
     }
-
-    bench!(all_flags, Flags::all());
-    bench!(no_flags, Flags::none());
 }
 
 fn runtime(criterion: &mut Criterion) {
-    scenario(criterion, "breakthrough.rg", vec![1, 22, 484, 11132]);
-    scenario(criterion, "ticTacToe.rg", vec![1, 9, 72, 504, 3024, 15120]);
+    scenario(
+        criterion,
+        "breakthrough",
+        &[".hrg.reuse.rg", ".hrg.rg", ".rbg.rg", ".rg", "-opt.rg"],
+    );
+    scenario(criterion, "ticTacToe", &[".kif.rg", ".rbg.rg", ".rg"]);
 }
 
 criterion_group!(benches, runtime);
