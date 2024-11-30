487fefbd3131f1560993facdb937592a880673d7
Radosław Miernik <radekmie@gmail.com>
2024-11-30 19:59:18 +0100

    Simplified translate-all-games.sh.

--- translate-all-games.sh
+++ translate-all-games.sh
@@ -11,16 +11,22 @@ cd interpreter_rust
 cargo build --release
 ./target/release/interpreter &> /dev/null || true # Preload.
 
+function translate {
+  local source=$1
+  local target=$2
+  local args=($3)
+
+  echo "$target\c"
+  time=$(date +%s%N)
+  timeout --foreground 120 ./target/release/interpreter source "${args[@]}" "${source}" > "${target}"
+  time=$((($(date +%s%N) - $time) / 1000000))
+  echo " ${time}ms"
+}
+
 rm -f examples/*.*.rg
-for game in ../examples/*.hrg; do
-  echo "${game}.rg\c"       && time=$(date +%s%N) && timeout --foreground 120 ./target/release/interpreter source                   "$game" > "${game}.rg"       && time=$((($(date +%s%N) - $time) / 1000000)) && echo " ${time}ms"
-  echo "${game}.reuse.rg\c" && time=$(date +%s%N) && timeout --foreground 120 ./target/release/interpreter source --reuse-functions "$game" > "${game}.reuse.rg" && time=$((($(date +%s%N) - $time) / 1000000)) && echo " ${time}ms"
-done
-
-for game in ../examples/*.kif; do
-  echo "${game}.rg\c" && time=$(date +%s%N) && timeout --foreground 120 ./target/release/interpreter source "$game" > "${game}.rg" && time=$((($(date +%s%N) - $time) / 1000000)) && echo " ${time}ms"
-done
-
-for game in ../examples/*.rbg; do
-  echo "${game}.rg\c" && time=$(date +%s%N) && timeout --foreground 120 ./target/release/interpreter source "$game" > "${game}.rg" && time=$((($(date +%s%N) - $time) / 1000000)) && echo " ${time}ms"
+for game in ../examples/*.{hrg,kif,rbg}; do
+  translate "${game}" "${game}.rg" ""
+  if [[ $game == *.hrg ]] ; then
+    translate "${game}" "${game}.reuse.rg" "--reuse-functions"
+  fi
 done
