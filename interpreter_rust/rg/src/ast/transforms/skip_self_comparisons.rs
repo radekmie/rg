7ef84b8a180e8810a4e9bf3351a1670574c4d919
Jakub Ciesluk <323892@uwr.edu.pl>
2024-12-27 17:11:17 +0100

    Dont remove unreachable comparisons

--- interpreter_rust/rg/src/ast/transforms/skip_self_comparisons.rs
+++ interpreter_rust/rg/src/ast/transforms/skip_self_comparisons.rs
@@ -3,20 +3,13 @@ use std::sync::Arc;
 
 impl<Id: Clone + PartialEq> Game<Id> {
     pub fn skip_self_comparisons(&mut self) -> Result<(), Error<Id>> {
-        let mut unreachable = vec![];
         for edge in &mut self.edges {
             if let Label::Comparison { lhs, rhs, negated } = &edge.label {
-                if lhs.is_equal_reference(rhs) {
-                    if *negated {
-                        unreachable.push(edge.clone());
-                    } else {
-                        Arc::make_mut(edge).skip();
-                    }
+                if lhs.is_equal_reference(rhs) && !*negated {
+                    Arc::make_mut(edge).skip();
                 }
             }
         }
-
-        self.edges.retain(|edge| !unreachable.contains(edge));
 
         Ok(())
     }
