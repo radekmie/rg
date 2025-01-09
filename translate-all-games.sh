b7da027a83c0f4acf213db4c87b0a69381a91e73
Radosław Miernik <radekmie@gmail.com>
2025-01-09 23:34:29 +0100

    Added timeout argument to translate-all-games.sh.

--- translate-all-games.sh
+++ translate-all-games.sh
@@ -7,6 +7,8 @@
 
 set -e
 
+timeout=${1:-2s}
+
 cd interpreter_rust
 cargo build --release
 ./target/release/interpreter &> /dev/null || true # Preload.
@@ -18,9 +20,18 @@ function translate {
 
   echo "$target\c"
   time=$(date +%s%N)
-  timeout --foreground 120 ./target/release/interpreter source "${args[@]}" "${source}" > "${target}"
-  time=$((($(date +%s%N) - $time) / 1000000))
-  echo " ${time}ms"
+
+  set +e
+  timeout --foreground $timeout ./target/release/interpreter source "${args[@]}" "${source}" > "${target}"
+  timeout_code=$?
+  set -e
+
+  if [ $timeout_code -eq 124 ] ; then
+    echo " \033[01;31mtimeout\033[00m"
+  else
+    time=$((($(date +%s%N) - $time) / 1000000))
+    echo " \033[01;32m${time}ms\033[00m"
+  fi
 }
 
 rm -f games/**/*.*.rg
