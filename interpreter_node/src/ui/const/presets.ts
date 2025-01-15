9547f2a97c292dec2f5948269fbfb0e00147b3c1
MSz <marek.esz@gmail.com>
2025-01-15 18:04:22 +0100

    Rename and remove exotic variants

--- interpreter_node/src/ui/const/presets.ts
+++ interpreter_node/src/ui/const/presets.ts
@@ -1,38 +1,24 @@
-import AmazonsNaiveHrg from 'bundle-text:../../../../games/hrg/amazons-naive.hrg';
-import AmazonsSmartHrg from 'bundle-text:../../../../games/hrg/amazons-smart.hrg';
+import AmazonsHrg from 'bundle-text:../../../../games/hrg/amazons.hrg';
 import AtaxxHrg from 'bundle-text:../../../../games/hrg/ataxx.hrg';
 import BreakthroughHrg from 'bundle-text:../../../../games/hrg/breakthrough.hrg';
 import Connect4Hrg from 'bundle-text:../../../../games/hrg/connect4.hrg';
-import DiceThrowCompareHrg from 'bundle-text:../../../../games/hrg/dice-throw-compare.hrg';
-import DiceThrowGuessHrg from 'bundle-text:../../../../games/hrg/dice-throw-guess.hrg';
+import DiceThrowCompareHrg from 'bundle-text:../../../../games/hrg/diceThrowCompare.hrg';
+import DiceThrowGuessHrg from 'bundle-text:../../../../games/hrg/diceThrowGuess.hrg';
 import GomokuFreestyleHrg from 'bundle-text:../../../../games/hrg/gomoku-freestyle.hrg';
 import KnightthroughHrg from 'bundle-text:../../../../games/hrg/knightthrough.hrg';
 import PentagoHrg from 'bundle-text:../../../../games/hrg/pentago.hrg';
-import SatSolverHrg from 'bundle-text:../../../../games/hrg/sat-solver.hrg';
-import ShortestPathHrg from 'bundle-text:../../../../games/hrg/shortest-path.hrg';
+import SatSolverHrg from 'bundle-text:../../../../games/hrg/satSolver.hrg';
+import ShortestPathHrg from 'bundle-text:../../../../games/hrg/shortestPath.hrg';
 import TicTacToeHrg from 'bundle-text:../../../../games/hrg/ticTacToe.hrg';
 import Connect4Gdl from 'bundle-text:../../../../games/kif/connect4.kif';
-import MontyHallGdl from 'bundle-text:../../../../games/kif/monty-hall.kif';
+import MontyHallGdl from 'bundle-text:../../../../games/kif/montyHall.kif';
 import TicTacToeGdl from 'bundle-text:../../../../games/kif/ticTacToe.kif';
-import $15puzzleRbg from 'bundle-text:../../../../games/rbg/15puzzle.rbg';
 import AmazonsRbg from 'bundle-text:../../../../games/rbg/amazons.rbg';
 import AmazonsSplit2Rbg from 'bundle-text:../../../../games/rbg/amazons_split2.rbg';
-import AmazonsSplit2aRbg from 'bundle-text:../../../../games/rbg/amazons_split2a.rbg';
-import AmazonsSplit3Rbg from 'bundle-text:../../../../games/rbg/amazons_split3.rbg';
-import AmazonsSplit5Rbg from 'bundle-text:../../../../games/rbg/amazons_split5.rbg';
-import AmazonsSplit5plusRbg from 'bundle-text:../../../../games/rbg/amazons_split5plus.rbg';
 import ArimaaRbg from 'bundle-text:../../../../games/rbg/arimaa.rbg';
 import ArimaaFixedPositionRbg from 'bundle-text:../../../../games/rbg/arimaa_fixedPosition.rbg';
 import ArimaaSplitRbg from 'bundle-text:../../../../games/rbg/arimaa_split.rbg';
 import BreakthroughRbg from 'bundle-text:../../../../games/rbg/breakthrough.rbg';
-import Breakthrough10x10Rbg from 'bundle-text:../../../../games/rbg/breakthrough_10x10.rbg';
-import Breakthrough11x11Rbg from 'bundle-text:../../../../games/rbg/breakthrough_11x11.rbg';
-import Breakthrough12x12Rbg from 'bundle-text:../../../../games/rbg/breakthrough_12x12.rbg';
-import Breakthrough5x5Rbg from 'bundle-text:../../../../games/rbg/breakthrough_5x5.rbg';
-import Breakthrough6x6Rbg from 'bundle-text:../../../../games/rbg/breakthrough_6x6.rbg';
-import Breakthrough7x7Rbg from 'bundle-text:../../../../games/rbg/breakthrough_7x7.rbg';
-import Breakthrough9x9Rbg from 'bundle-text:../../../../games/rbg/breakthrough_9x9.rbg';
-import BreakthroughSplitRbg from 'bundle-text:../../../../games/rbg/breakthrough_split.rbg';
 import BreakthruRbg from 'bundle-text:../../../../games/rbg/breakthru.rbg';
 import BreakthruSplitRbg from 'bundle-text:../../../../games/rbg/breakthru_split.rbg';
 import CanadianDraughtsRbg from 'bundle-text:../../../../games/rbg/canadianDraughts.rbg';
@@ -51,8 +37,6 @@ import Connect6SplitRbg from 'bundle-text:../../../../games/rbg/connect6_split.r
 import DoubleChessRbg from 'bundle-text:../../../../games/rbg/doubleChess.rbg';
 import EnglishDraughtsRbg from 'bundle-text:../../../../games/rbg/englishDraughts.rbg';
 import EnglishDraughtsSplitRbg from 'bundle-text:../../../../games/rbg/englishDraughts_split.rbg';
-import FoxAndHounds10x10Rbg from 'bundle-text:../../../../games/rbg/foxAndHounds-10x10.rbg';
-import FoxAndHounds12x12Rbg from 'bundle-text:../../../../games/rbg/foxAndHounds-12x12.rbg';
 import FoxAndHoundsRbg from 'bundle-text:../../../../games/rbg/foxAndHounds.rbg';
 import GessRbg from 'bundle-text:../../../../games/rbg/gess.rbg';
 import GoRbg from 'bundle-text:../../../../games/rbg/go.rbg';
@@ -60,25 +44,14 @@ import GoConstsumRbg from 'bundle-text:../../../../games/rbg/go_constsum.rbg';
 import GoNopassRbg from 'bundle-text:../../../../games/rbg/go_nopass.rbg';
 import GomokuFreestyleRbg from 'bundle-text:../../../../games/rbg/gomoku_freeStyle.rbg';
 import GomokuStandardRbg from 'bundle-text:../../../../games/rbg/gomoku_standard.rbg';
-import GomokuStandard11x11Rbg from 'bundle-text:../../../../games/rbg/gomoku_standard_11x11.rbg';
-import GomokuStandard13x13Rbg from 'bundle-text:../../../../games/rbg/gomoku_standard_13x13.rbg';
 import HexRbg from 'bundle-text:../../../../games/rbg/hex.rbg';
-import Hex10x10Rbg from 'bundle-text:../../../../games/rbg/hex_10x10.rbg';
-import Hex5x5Rbg from 'bundle-text:../../../../games/rbg/hex_5x5.rbg';
-import Hex6x6Rbg from 'bundle-text:../../../../games/rbg/hex_6x6.rbg';
-import Hex7x7Rbg from 'bundle-text:../../../../games/rbg/hex_7x7.rbg';
-import Hex8x8Rbg from 'bundle-text:../../../../games/rbg/hex_8x8.rbg';
 import Hex9x9Rbg from 'bundle-text:../../../../games/rbg/hex_9x9.rbg';
 import InternationalDraughtsRbg from 'bundle-text:../../../../games/rbg/internationalDraughts.rbg';
 import KnightthroughRbg from 'bundle-text:../../../../games/rbg/knightthrough.rbg';
-import KnightthroughSplitRbg from 'bundle-text:../../../../games/rbg/knightthrough_split.rbg';
 import PaperSoccerRbg from 'bundle-text:../../../../games/rbg/paperSoccer.rbg';
 import PentagoRbg from 'bundle-text:../../../../games/rbg/pentago.rbg';
 import PentagoSplitRbg from 'bundle-text:../../../../games/rbg/pentago_split.rbg';
 import ReversiRbg from 'bundle-text:../../../../games/rbg/reversi.rbg';
-import Reversi10x10Rbg from 'bundle-text:../../../../games/rbg/reversi_10x10.rbg';
-import Reversi4x4Rbg from 'bundle-text:../../../../games/rbg/reversi_4x4.rbg';
-import Reversi6x6Rbg from 'bundle-text:../../../../games/rbg/reversi_6x6.rbg';
 import SkirmishRbg from 'bundle-text:../../../../games/rbg/skirmish.rbg';
 import TheMillGameRbg from 'bundle-text:../../../../games/rbg/theMillGame.rbg';
 import TheMillGameSplitRbg from 'bundle-text:../../../../games/rbg/theMillGame_split.rbg';
@@ -93,14 +66,8 @@ import { Language } from '../../types';
 
 // prettier-ignore
 const games = [
-  ['15puzzle', $15puzzleRbg, Language.rbg],
-  ['Amazons (naive)', AmazonsNaiveHrg, Language.hrg],
-  ['Amazons (smart)', AmazonsSmartHrg, Language.hrg],
+  ['Amazons', AmazonsHrg, Language.hrg],
   ['Amazons (split2)', AmazonsSplit2Rbg, Language.rbg],
-  ['Amazons (split2a)', AmazonsSplit2aRbg, Language.rbg],
-  ['Amazons (split3)', AmazonsSplit3Rbg, Language.rbg],
-  ['Amazons (split5)', AmazonsSplit5Rbg, Language.rbg],
-  ['Amazons (split5+)', AmazonsSplit5plusRbg, Language.rbg],
   ['Amazons', AmazonsRbg, Language.rbg],
   ['Arimaa', ArimaaRbg, Language.rbg],
   ['Arimaa (fixed position)', ArimaaFixedPositionRbg, Language.rbg],
@@ -108,16 +75,8 @@ const games = [
   ['Ataxx', AtaxxHrg, Language.hrg],
   ['Breakthrough', BreakthroughHrg, Language.hrg],
   ['Breakthrough', BreakthroughRbg, Language.rbg],
-  ['Breakthrough (split)', BreakthroughSplitRbg, Language.rbg],
   ['Breakthrough', BreakthroughRg, Language.rg],
   ['Breakthrough (opt)', BreakthroughOptRg, Language.rg],
-  ['Breakthrough (5x5)', Breakthrough5x5Rbg, Language.rbg],
-  ['Breakthrough (6x6)', Breakthrough6x6Rbg, Language.rbg],
-  ['Breakthrough (7x7)', Breakthrough7x7Rbg, Language.rbg],
-  ['Breakthrough (9x9)', Breakthrough9x9Rbg, Language.rbg],
-  ['Breakthrough (10x10)', Breakthrough10x10Rbg, Language.rbg],
-  ['Breakthrough (11x11)', Breakthrough11x11Rbg, Language.rbg],
-  ['Breakthrough (12x12)', Breakthrough12x12Rbg, Language.rbg],
   ['Breakthru', BreakthruRbg, Language.rbg],
   ['Breakthru (split)', BreakthruSplitRbg, Language.rbg],
   ['CanadianDraughts', CanadianDraughtsRbg, Language.rbg],
@@ -141,8 +100,6 @@ const games = [
   ['EnglishDraughts', EnglishDraughtsRbg, Language.rbg],
   ['EnglishDraughts (split)', EnglishDraughtsSplitRbg, Language.rbg],
   ['FoxAndHounds', FoxAndHoundsRbg, Language.rbg],
-  ['FoxAndHounds (10x10)', FoxAndHounds10x10Rbg, Language.rbg],
-  ['FoxAndHounds (12x12)', FoxAndHounds12x12Rbg, Language.rbg],
   ['Gess', GessRbg, Language.rbg],
   ['Go', GoRbg, Language.rbg],
   ['Go (constsum)', GoConstsumRbg, Language.rbg],
@@ -150,28 +107,17 @@ const games = [
   ['Gomoku (freestyle)', GomokuFreestyleHrg, Language.hrg],
   ['Gomoku (freestyle)', GomokuFreestyleRbg, Language.rbg],
   ['Gomoku (standard)', GomokuStandardRbg, Language.rbg],
-  ['Gomoku (standard, 11x11)', GomokuStandard11x11Rbg, Language.rbg],
-  ['Gomoku (standard, 13x13)', GomokuStandard13x13Rbg, Language.rbg],
   ['Hex', HexRbg, Language.rbg],
-  ['Hex (5x5)', Hex5x5Rbg, Language.rbg],
-  ['Hex (6x6)', Hex6x6Rbg, Language.rbg],
-  ['Hex (7x7)', Hex7x7Rbg, Language.rbg],
-  ['Hex (8x8)', Hex8x8Rbg, Language.rbg],
   ['Hex (9x9)', Hex9x9Rbg, Language.rbg],
-  ['Hex (10x10)', Hex10x10Rbg, Language.rbg],
   ['InternationalDraughts', InternationalDraughtsRbg, Language.rbg],
   ['Knightthrough', KnightthroughHrg, Language.hrg],
   ['Knightthrough', KnightthroughRbg, Language.rbg],
-  ['Knightthrough (split)', KnightthroughSplitRbg, Language.rbg],
   ['Minimal', MinimalRg, Language.rg],
   ['MontyHall', MontyHallGdl, Language.gdl],
   ['PaperSoccer', PaperSoccerRbg, Language.rbg],
   ['Pentago', PentagoHrg, Language.hrg],
   ['Pentago', PentagoRbg, Language.rbg],
   ['Pentago (split)', PentagoSplitRbg, Language.rbg],
-  ['Reversi (4x4)', Reversi4x4Rbg, Language.rbg],
-  ['Reversi (6x6)', Reversi6x6Rbg, Language.rbg],
-  ['Reversi (10x10)', Reversi10x10Rbg, Language.rbg],
   ['Reversi', ReversiRbg, Language.rbg],
   ['SatSolver', SatSolverHrg, Language.hrg],
   ['ShortestPath', ShortestPathHrg, Language.hrg],
