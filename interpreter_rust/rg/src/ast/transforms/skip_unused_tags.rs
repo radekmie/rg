564871f27d992ec4f9d0b03f5a48bdaaab47353a
jkciesluk <323892@uwr.edu.pl>
2025-05-04 09:41:11 +0200

    Dont skip unused artificial tags

--- interpreter_rust/rg/src/ast/transforms/skip_unused_tags.rs
+++ interpreter_rust/rg/src/ast/transforms/skip_unused_tags.rs
@@ -1,12 +1,27 @@
 use crate::ast::analyses::ReachableNodes;
-use crate::ast::{Error, Game};
+use crate::ast::{Error, Game, Pragma};
+use std::collections::BTreeSet;
 use std::sync::Arc;
 
 impl Game<Arc<str>> {
     pub fn skip_unused_tags(&mut self) -> Result<(), Error<Arc<str>>> {
+        let artificial_tags: BTreeSet<_> = self
+            .pragmas
+            .iter()
+            .filter_map(|pragma| {
+                if let Pragma::ArtificialTag { tags, .. } = pragma {
+                    Some(tags)
+                } else {
+                    None
+                }
+            })
+            .flatten()
+            .collect();
+
         let reachable_nodes = self.analyse::<ReachableNodes>(false);
         for edge in &mut self.edges {
-            if (edge.label.is_tag() || edge.label.is_tag_variable())
+            if (edge.label.is_tag_and(|tag| !artificial_tags.contains(tag))
+                || edge.label.is_tag_variable())
                 && !reachable_nodes
                     .get(&edge.lhs)
                     .is_some_and(|reachable| *reachable)
@@ -42,6 +57,15 @@ mod test {
         "begin, end: ? t1 -> t2;
         t1, t2: ;
         t2, t3: ;"
+    );
+
+    test_transform!(
+        skip_unused_tags,
+        reachability_artificial,
+        "begin, end: ? t1 -> t2;
+        t1, t2: $ 1;
+        t2, t3: $ 2;
+        @artificialTag 1 2;"
     );
 
     test_transform!(
