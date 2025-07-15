ef0415adf81df091c69ec289ca659fbf2e826a6f
MSz <marek.esz@gmail.com>
2025-07-15 14:19:03 +0200

    Cleanup fixes

--- games/py/generateAll.sh
+++ games/py/generateAll.sh
@@ -1,11 +1,15 @@
- #!/bin/bash
+#!/bin/bash
 
 SRC_DIR="sources"
 OUT_DIR="hrg"
 
+rm "$OUT_DIR"/*
+
 for script in "$SRC_DIR"/*.py; do
-    baseName=$(basename "$script")
-    filename=$(basename "$script" .py)
-    python3 "$script" > "$OUT_DIR/$filename.hrg" 2>&1
-    echo "Saved $baseName"
+    fileName=$(basename "$script" .py)
+    if [[ "$fileName" == "lineGames" ]]; then
+        continue
+    fi
+    python3 "$script" > "$OUT_DIR/${fileName}.hrg" 2>&1
+    echo "Saved ${fileName}.hrg"
 done
